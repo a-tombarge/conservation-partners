753f6c7d39e47e04920486773c45dd1d94ad9b06
Alisande Tombarge <amto236@uky.edu>
2018-03-11 15:49:14 -0400

    move script over to a js file

--- js/app.js
+++ js/app.js
@@ -1,7 +1,28 @@
 (function (){
 
-    // L.mapbox.accessToken = 'pk.eyJ1IjoiYS10b21iYXJnZSIsImEiOiJjamRkdXM3ZG0wM3M2MnFtMjZsMnVibDB5In0.iJk8JWQpFOjMnWbgfYwRzw';
-    // var map = L.mapbox.map('map', 'mapbox.streets')
-    // .setView([40, -74.50], 9);  
+  L.mapbox.accessToken = 'pk.eyJ1IjoiYS10b21iYXJnZSIsImEiOiJjamRkdXM3ZG0wM3M2MnFtMjZsMnVibDB5In0.iJk8JWQpFOjMnWbgfYwRzw';
+    
+  var map = L.mapbox.map('map', 'mapbox.streets-basic', {
+      center: [37.85, -80.21],
+      zoom: 7.5,
+      minZoom: 7,
+      maxZoom: 14,
+      zoomControl: false
+     // maxBounds: L.latLngBounds([39, -85], [36, -70])
+   
+  });
 
-}) //end of master function
+  L.control.zoom({
+    position:'topright'
+  }).addTo(map);
+ 
+  //Using omnivore to load my data layers
+
+  omnivore.geojson('data/cp-easements-list1.geojson').addTo(map);
+  
+  
+  omnivore.topojson('data/dcr-easements-topojson.json').addTo(map); 
+
+  omnivore.geojson('data/va-counties.geojson').addTo(map);
+
+})(); //end of master function
