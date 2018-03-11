916347cd948b5e17833f4bdeaa69aa7446d79a1d
Alisande Tombarge <amto236@uky.edu>
2018-03-11 15:16:11 -0400

    set basemap styles

--- js/app.js
+++ js/app.js
@@ -1,20 +1,7 @@
 (function (){
 
-    L.mapbox.accessToken = 'pk.eyJ1Ijoicmdkb25vaHVlIiwiYSI6Im5Ua3F4UzgifQ.PClcVzU5OUj17kuxqsY_Dg';
-  
-    var map = L.mapbox.map('map', 'mapbox.light', {
-      zoomSnap: .1,
-      center: [-.23, 37.8],
-      zoom: 7,
-      minZoom: 6,
-      maxZoom: 9,
-      maxBounds: L.latLngBounds([-6.22, 27.72], [5.76, 47.83])
-    });
-
-
-
-
-
-
+    // L.mapbox.accessToken = 'pk.eyJ1IjoiYS10b21iYXJnZSIsImEiOiJjamRkdXM3ZG0wM3M2MnFtMjZsMnVibDB5In0.iJk8JWQpFOjMnWbgfYwRzw';
+    // var map = L.mapbox.map('map', 'mapbox.streets')
+    // .setView([40, -74.50], 9);  
 
 }) //end of master function
