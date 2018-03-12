0917ffe066e728001cee2d5690eb4a277f2b22bc
Alisande T <32623574+a-tombarge@users.noreply.github.com>
2018-03-11 20:35:11 -0400

    load DCR data

--- js/app.js
+++ js/app.js
@@ -1,28 +1,57 @@
-(function (){
+(function () {
 
   L.mapbox.accessToken = 'pk.eyJ1IjoiYS10b21iYXJnZSIsImEiOiJjamRkdXM3ZG0wM3M2MnFtMjZsMnVibDB5In0.iJk8JWQpFOjMnWbgfYwRzw';
-    
+
   var map = L.mapbox.map('map', 'mapbox.streets-basic', {
-      center: [38.016, -79.69],
-      zoom: 7.5,
-      minZoom: 7,
-      maxZoom: 14,
-      zoomControl: false,
-      zoomSnap: 1
-     // maxBounds: L.latLngBounds([39, -85], [36, -70])
-   
+    center: [38.016, -79.69],
+    zoom: 7.5,
+    minZoom: 7,
+    maxZoom: 14,
+    zoomControl: false,
+    zoomSnap: 1
+    // maxBounds: L.latLngBounds([39, -85], [36, -70])
+
   });
 
   L.control.zoom({
-    position:'topright'
+    position: 'topright'
   }).addTo(map);
- 
+
+
 
   
-  $.getJSON('data/cp-easements-list1.geojson', function(cpEasements){
+
+  $.getJSON('data/va-counties.geojson', function (counties) {
+    //console.log(counties);
+
+    L.geoJson(counties, {
+      style: function (feature) {
+        return {
+          color: '#999999',
+          weight: 1.5,
+          fillOpacity: 0
+        };
+      }
+    }).addTo(map);
+  })
+
+  $.getJSON('data/dcr-easements.json', function (dcrEasements) {
+    L.geoJson(dcrEasements, {
+      style: function (feature) {
+        return {
+          color: '#9feaf9',
+          weight: 0.3,
+          fillOpacity: 0.5,
+          fillColor: '#9feaf9'
+        };
+      }
+    }).addTo(map);
+  })
+
+  $.getJSON('data/cp-easements-list1.geojson', function (cpEasements) {
     //console.log(cpEasements);
     L.geoJson(cpEasements, {
-      style: function(feature){
+      style: function (feature) {
         return {
           color: '#fc2a2a',
           weight: 0.5,
@@ -33,35 +62,7 @@
     }).addTo(map);
   })
 
-  $.getJSON('data/va-counties.geojson', function(counties){
-    //console.log(counties);
-
-    L.geoJson(counties, {
-      style: function(feature) {
-        return {
-        color: '#999999',
-        weight: 1.5,
-        fillOpacity: 0 
-        };
-      }
-    }).addTo(map);
-  })
-
-  $.getJSON('data/dcr-easements-topojson.json', function(dcrEasements){
-    L.geoJson(dcrEasements, {
-      style: function(feature) {
-        return {
-        color: '#e3fce5',
-        weight: 0.5,
-        fillOpacity: 1,
-        fillColor: '#e3fce5'
-        };
-      }
-    }).addTo(map);
-  })
-  
-  
   //omnivore.topojson('data/dcr-easements-topojson.json').addTo(map); 
 
- 
+
 })(); //end of master function
