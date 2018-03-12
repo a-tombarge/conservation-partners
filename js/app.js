150aa9ca5ace308404b34af5b5f64f283b24f321
Alisande T <32623574+a-tombarge@users.noreply.github.com>
2018-03-11 20:06:41 -0400

    re-load data layers and change map settings

--- js/app.js
+++ js/app.js
@@ -3,11 +3,12 @@
   L.mapbox.accessToken = 'pk.eyJ1IjoiYS10b21iYXJnZSIsImEiOiJjamRkdXM3ZG0wM3M2MnFtMjZsMnVibDB5In0.iJk8JWQpFOjMnWbgfYwRzw';
     
   var map = L.mapbox.map('map', 'mapbox.streets-basic', {
-      center: [37.85, -80.21],
+      center: [38.016, -79.69],
       zoom: 7.5,
       minZoom: 7,
       maxZoom: 14,
-      zoomControl: false
+      zoomControl: false,
+      zoomSnap: 1
      // maxBounds: L.latLngBounds([39, -85], [36, -70])
    
   });
@@ -16,13 +17,30 @@
     position:'topright'
   }).addTo(map);
  
-  //Using omnivore to load my data layers
 
-  omnivore.geojson('data/cp-easements-list1.geojson').addTo(map);
   
-  
-  omnivore.topojson('data/dcr-easements-topojson.json').addTo(map); 
+  $.getJSON('data/cp-easements-list1.geojson', function(cpEasements){
+    //console.log(cpEasements);
+    L.geoJson(cpEasements).addTo(map);
 
-  omnivore.geojson('data/va-counties.geojson').addTo(map);
+  })
 
+  $.getJSON('data/va-counties.geojson', function(counties){
+    //console.log(counties);
+
+    L.geoJson(counties, {
+      style: function(feature) {
+        return {
+        color: '#A9A9A9',
+        weight: 1.5,
+        fillOpacity: 0 
+        };
+      }
+    }).addTo(map);
+
+  })
+
+  //omnivore.topojson('data/dcr-easements-topojson.json').addTo(map); 
+
+ 
 })(); //end of master function
