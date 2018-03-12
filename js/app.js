a8be4d2a9363d01983d3f811f60d7134978a5403
Alisande Tombarge <amto236@uky.edu>
2018-03-12 18:55:51 -0400

    edit page layout and basemaps; create functions for each datalayer; create tooltips for CP easements

--- js/app.js
+++ js/app.js
@@ -1,18 +1,23 @@
 (function () {
 
-  L.mapbox.accessToken = 'pk.eyJ1IjoiYS10b21iYXJnZSIsImEiOiJjamRkdXM3ZG0wM3M2MnFtMjZsMnVibDB5In0.iJk8JWQpFOjMnWbgfYwRzw';
-
-  var map = L.mapbox.map('map', 'mapbox.streets-basic', {
+  //set map location, zoom
+  var map = L.map('map', {
     center: [38.016, -79.69],
     zoom: 7.5,
     minZoom: 7,
     maxZoom: 14,
     zoomControl: false,
     zoomSnap: 1
-    // maxBounds: L.latLngBounds([39, -85], [36, -70])
+  })
 
-  });
+  //add basemap
+  var tiles = L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/rastertiles/light_all/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
+    subdomains: 'abcd',
+    maxZoom: 19
+  }).addTo(map);
 
+  //re-position zoomControl
   L.control.zoom({
     position: 'topright'
   }).addTo(map);
@@ -23,47 +28,104 @@
       cpEasementsData = $.getJSON('data/cp-easements-list1.geojson');
 
   // use jQuery promise to wait until they're all loaded
-  $.when(countiesData, easementsData, cpEasementsData).done(ready);
+  $.when(countiesData, easementsData, cpEasementsData).done(drawMap);
 
-  // function called when data are loaded and ready 
-  // essentially the drawMap function?
-  function ready(countiesData, easementsData, cpEasementsData) {
+  // drawMap function called when datalayers are loaded and ready 
+  function drawMap(countiesData, easementsData, cpEasementsData) {
 
-      // data are all now loaded and accessible within this function
+    drawCounties(countiesData);
+    drawEasements(easementsData);
+    drawcpEasements(cpEasementsData);
 
-      L.geoJson(countiesData, {
-        style: function (feature) {
-          return {
-            color: '#999999',
-            weight: 1.5,
-            fillOpacity: 0
-          };
-        }
-      }).addTo(map);
+  } //end of drawMap function
 
-      L.geoJson(easementsData, {
-        style: function (feature) {
-          return {
-            color: '#9feaf9',
-            weight: 0.3,
-            fillOpacity: 0.5,
-            fillColor: '#9feaf9'
-          };
-        }
-      }).addTo(map);
 
-      L.geoJson(cpEasementsData, {
-        style: function (feature) {
-          return {
-            color: '#fc2a2a',
-            weight: 0.5,
-            fillOpacity: 1,
-            fillColor: '#fc2a2a'
-          };
-        }
-      }).addTo(map);
+  function drawCounties(countiesData) {
+
+    var countiesList = []
+
+    var counties = L.geoJson(countiesData, {
+      style: function (feature) {
+        return {
+          color: '#999999',
+          weight: 1.5,
+          fillOpacity: 0
+        };
+      },
+      filter: function(feature) {
+        countiesList.push(feature.properties.NAME)
+        return feature
+      }
+    }).addTo(map);
+
+    //searchByCounty(counties, countiesList)
+
+  } //end of drawCounties function 
+
+     
+  function drawEasements(easementsData) {
+
+    L.geoJson(easementsData, {
+      style: function (feature) {
+        return {
+          color: '#9feaf9',
+          weight: 0.3,
+          fillOpacity: 0.5,
+          fillColor: '#9feaf9'
+        };
+      }
+    }).addTo(map);
+
+
+  }
+  
+  //add CP easements to map
+  function drawcpEasements(cpEasementsData) {
+
+    var cpEasements = L.geoJson(cpEasementsData, {
+      style: function (feature) {
+        return {
+          color: '#fc2a2a',
+          weight: 0.5,
+          fillOpacity: 1,
+          fillColor: '#fc2a2a'
+        };
+      },
+      onEachFeature(feature, layer) {
+        //console.log(feature.properties.cp_listacr)
+        //build CP easement tooltip
+        var toolTipInfo = "<b>County:</b> " + feature.properties.county + "<br><b>Acreage:</b> " + 
+                          feature.properties.cp_listacr + "<br><b>Holder:</b> " + feature.properties.cp_listhol +
+                          "<br><b>Conservation Values:</b> " + feature.properties.cp_listcon + 
+                          "<br><b>Year Placed in Easement:</b> " + feature.properties.year;
+        layer.bindTooltip(toolTipInfo, {
+          sticky: true,
+          toolTipAnchor: [200,200]
+        });
+      }
+    }).addTo(map);
+
+    //filterByYear(cpEasements)
 
   }
 
+  // function filterByYear(cpEasements) {
+
+  //   // select the UI slider
+  //   $("#slider").on("input change", function(res) {
+      
+  //     // code will repeat with each slider change
+
+  //     console.log(res.target.value)
+
+  //     cpEasements.eachLayer(function(layer) {
+  //       console.log(layer.feature.properties)
+  //       // if(feature properties year is equal the current selected value)
+  //       // then show easement
+  //       // else hide easement
+  //     })
+  //   });
+  // }
+ 
  
 })(); //end of master function
