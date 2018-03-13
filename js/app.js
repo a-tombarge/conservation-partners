d1019cecc55f58096d905518f45e30006dfd22be
a-tombarge <amto236@uky.edu>
2018-03-13 16:37:46 -0400

    set up array for my years for the slider

--- js/app.js
+++ js/app.js
@@ -84,22 +84,25 @@
         
         layer.bindPopup(popupInfo);          
       
-        // //when mousing over layer
-        // layer.on('mouseover', function() {
-        //   //change the stroke color
-        //   layer.setStyle({
-        //     color: '#ffff4d',
-        //     weight: 2
-        //   }).bringToFront();
-        // });
+        //when clicking on layer
+        layer.on('click', function() {
+          //change the stroke color
+          layer.setStyle({
+            color: '#ffff4d',
+            weight: 2
+          }).bringToFront();
+        });
         
-        // //when mousing off layer
-        // layer.on('mouseout', function() {
-        //   // reset the layer style to its original stroke color
-        //   layer.setStyle({
-        //     color: '#ffc966'
-        //   });
-        // });
+        //when mousing off layer
+        layer.on('mouseout', function() {
+          // reset the layer style to its original stroke color
+          layer.setStyle({
+            color: '#ffc966',
+            weight: 0.3,
+            fillOpacity: 0.5,
+            fillColor: '#ffc966'
+          });
+        });
       }  
     }).addTo(map);
 
@@ -148,14 +151,20 @@
           });
         });
       }
-    }).addTo(map);
-    
+    }).addTo(map); 
+  
     filterByYear(cpEasements)
   
   } //end of drawcpEasements function
 
+  //slider filtering by year and totaling acreage per year
   function filterByYear(cpEasements) {
-
+    
+    //creating slider control and adding it to my map
+    var sliderControl = L.control({
+          position: 'centerleft'
+        });
+    
     sliderControl.onAdd = function (map) {
     
       var controls = L.DomUtil.get("slider");
@@ -169,22 +178,43 @@
     // add it to the map
     sliderControl.addTo(map);
 
-    // select the UI slider
-    $("#slider").on("input change", function(res) {
-      
-      // code will repeat with each slider change
+    
+    //array to store my years
+    var years = [];
 
-      console.log(res.target.value)
-
-      cpEasements.eachLayer(function(layer) {
-        
-        console.log(layer.feature.properties)
-        // if(feature properties year is equal the current selected value)
-        // then show easement
-        // else hide easement
-      })
+    feature.properties.forEach(function(easementYear) {
+      for (var property in feature.properties) {
+        if(property === "year") {
+          years.push(Number(feature.properties[property]));
+        }
+      }
     });
-  }
- 
- 
+  
+    console.log(years);
+
+
+    // // select the UI slider
+    // $("#slider").on("input change", function(res) {
+      
+    //   // code will repeat with each slider change
+
+    //   console.log(res.target.value)
+
+    //   cpEasements.eachLayer(function(layer) {
+        
+    //     // console.log(layer.feature.properties)
+         
+    //     // if (layer.feature.properties.year = currentYear) {
+    //     //   // if(feature properties year is equal the current selected value)
+    //     //   // then show easement
+    //     // } else {
+    //     //  // else hide easement 
+    //     // }
+                
+    //   })
+    // });
+  } //end filterByYear function
+
+
+
 })(); //end of master function
